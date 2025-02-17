10a175ffd45b729232f277b7b21d74c52bb280f5
KAJAL SRIVASTAVA <73694048+kajal1913061@users.noreply.github.com>
2025-02-17 20:19:03 +0000

    Array methods

--- array.js
+++ array.js
@@ -29,15 +29,48 @@ const newArr = myArr.join()
 
 // slice, splice
 
-console.log("A ", myArr);  //A  [ 0, 1, 2, 3, 4, 5 ]
+//console.log("A ", myArr);  //A  [ 0, 1, 2, 3, 4, 5 ]
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);  //[ 1, 2 ]
-console.log("B ", myArr);  //B  [ 0, 1, 2, 3, 4, 5 ]
+// console.log(myn1);  //[ 1, 2 ]
+// console.log("B ", myArr);  //B  [ 0, 1, 2, 3, 4, 5 ]
 
 
 const myn2 = myArr.splice(1, 3)
 
-console.log(myn2);     //[ 1, 2, 3 ]
-console.log("C ", myArr); // C  [ 0, 4, 5 ]
+// console.log(myn2);     //[ 1, 2, 3 ]
+// console.log("C ", myArr); // C  [ 0, 4, 5 ]
+
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+ const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
